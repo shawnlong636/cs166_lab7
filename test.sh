7600a4923c9d252263af4d705b5bd2b18a51060b
Shawn Long <slong024@wch133-25.cs.ucr.edu>
2022-03-03 16:47:56 -0800

    Fixed erros in test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Creating sequence..."
-cat <(echo 'CREATE SEQUENCE part_number_seq  START WITH 50000;')|psql -p $PGPORT $DB_NAME
+cat <(echo 'CREATE SEQUENCE part_number_seq  START WITH 50000;')|psql -h localhost -p $PGPORT slong024_DB
 
 echo "Creating trigger and procedure..."
-psql -p $PGPORT $DB_NAME < triggers.sql
+psql -h localhost -p $PGPORT slong024_DB < triggers.sql
 
 echo "Testing insert..."
-cat <(echo "Insert into part_nyc(supplier, color, on_hand, descr) Values (0, 0, 20, 'Description')")|psql -p $PGPORT $DB_NAME
-cat <(echo 'SELECT part_number FROM part_nyc WHERE on_hand=20;')|psql -p $PGPORT $DB_NAME
+cat <(echo "Insert into part_nyc(supplier, color, on_hand, descr) Values (0, 0, 20, 'Description')")|psql -h localhost -p $PGPORT slong024_DB
+cat <(echo 'SELECT part_number FROM part_nyc WHERE on_hand=20;')|psql -h localhost -p $PGPORT slong024_DB
