7d7bc11ab92de8b14e54c0c01c2261f87ea555a8
Shawn Long <slong024@wch133-25.cs.ucr.edu>
2022-03-03 17:22:24 -0800

    Implemented procedure

--- triggers.sql
+++ triggers.sql
@@ -1,9 +1,13 @@
-CREATE LANGUAGE plpgsql;
-CREATE OR REPLACE FUNCTION addPartNo () AS '
-	BEGIN
-		-- Do Something
-	END;
-' LANGUAGE 'plpgsql';
+--CREATE LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION addPartNo () 
+RETURNS "trigger" AS
+$BODY$
+BEGIN
+	New.part_number:=nextval('part_number_seq');
+	Return NEW;
+END;
+$BODY$
+LANGUAGE 'plpgsql' VOLATILE;
 
 CREATE TRIGGER insertPart BEFORE INSERT ON part_nyc
-	FOR EACHN ROW EXECUTE PROCEDURE  addPartNo();
+	FOR EACH ROW EXECUTE PROCEDURE  addPartNo();
